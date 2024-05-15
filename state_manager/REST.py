0b73a55d39109fdeeb6da0eafb864c31871df232
rafixxx4k <rafa.szubert@gmail.com>
2024-05-15 14:49:55 +0200

    Refactor REST function to include message handling and broadcasting

--- state_manager/REST.py
+++ state_manager/REST.py
@@ -1,2 +1,25 @@
-def rest():
-    print("REST")
+from constants import *
+import random
+from communication.broadcast import broadcast
+
+# from main import MPI, comm, rank, size, Z, N, P, S, animal_type
+
+
+def rest(args):
+    Z, N, P, S, MPI, comm, rank, size, animal_type = args
+    status = MPI.Status()
+    while True:
+        if comm.Iprobe(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status):
+            message = comm.recv(
+                source=status.Get_source(), tag=status.Get_tag(), status=status
+            )
+            if status.Get_tag() == REQ:
+                print("my rank is", rank, " and REQ from", status.Get_source())
+                comm.send(message, dest=status.Get_source(), tag=ACK)
+            else:
+                pass
+        if random.random() < 0.1:
+            my_new_glade = random.randint(0, P - 1)
+            print("my rank is", rank, " and i send REQ to all")
+            broadcast(comm, REQ, my_new_glade, rank, size)
+            return WAIT
